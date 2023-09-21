98d2fc5627f9aee1e8639ed237d5d653c9d095b8
Hamp001 <124536626+Hamp001@users.noreply.github.com>
2023-09-20 20:53:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import server
 
 
 sta_if = network.WLAN(network.STA_IF); sta_if.active(True)
-sta_if.connect("INFINITUMpju2","f7132b6deb")
+sta_if.connect("nombre_de_red","contra")
 sta_if.isconnected()
 
 # Configura una nueva contraseña para WebREPL
